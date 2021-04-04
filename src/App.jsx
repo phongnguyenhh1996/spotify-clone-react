0727e65d8cff0e57a86626d914cf6241dce1e4ad
Phong NHH <phongnguyenhh.1996@gmail.com>
2021-04-04 15:49:59 +0700

    check husky

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './app.scss'
 import PropTypes from 'prop-types'
 
-const App = ({title}) =>  <div className="title">{title}</div>
+const App = ({title}) => <div className="title">{title}</div>
 
 App.propTypes = {
   title: PropTypes.string.isRequired,
