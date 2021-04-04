a10b9ac46987de2d8439417c2b47b982378f3897
Phong NHH <phongnguyenhh.1996@gmail.com>
2021-04-04 15:34:17 +0700

    init source: webpack, react, eslint, stylelint, prettier, scss, hot reload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './app.scss'
+import PropTypes from 'prop-types'
+
+const App = ({title}) =>  <div className="title">{title}</div>
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import App from './App'
+
+ReactDOM.render(<App title="Hello world 2!" />, document.getElementById('root'))
