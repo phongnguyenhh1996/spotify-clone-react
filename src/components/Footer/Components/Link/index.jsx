5d54d2f847cee0f1af79f2fd621cc7a548ecbe77
unknown <hieuh5355@gmail.com>
2021-04-14 10:34:17 +0700

    split component LinkFooter in footer

--- src/components/Footer/Components/Link/index.jsx
+++ src/components/Footer/Components/Link/index.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
+import './style.scss'
 
 function LinkFooter(props) {
   const {title, listLink} = props
   return (
-    <div className="footer__company">
-      <h3 className="footer__title">{title}</h3>
-      <ul className="footer__list">
+    <div className="link-list">
+      <h3 className="link-list__title">{title}</h3>
+      <ul className="link-list__list">
         {listLink.map((item) => (
-          <li className="footer__li" key={item.id}>
-            <Link className="footer__link" to="/">
+          <li className="link-list__li" key={item.id}>
+            <Link className="link-list__link" to="/">
               {item.link}
             </Link>
           </li>
@@ -23,4 +24,5 @@ LinkFooter.propTypes = {
   title: PropTypes.string.isRequired,
   listLink: PropTypes.arrayOf(PropTypes.object).isRequired,
 }
+
 export default LinkFooter
